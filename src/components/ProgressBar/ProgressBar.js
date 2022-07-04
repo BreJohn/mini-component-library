78a46ff09fb6895ef1b1266a63d6f954e995429a
gbregiannis <gbregiannis@gmail.com>
2022-07-04 16:37:09 +0300

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,80 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  // const height = (size) => {
+  //   if(size === 'small') {
+  //     return 8;
+  //   } else if(size === 'medium') {
+  //     return 12;
+  //   }
+  //   return 24;
+  // }
+
+  // const padding = size === 'large'? 2 : 0;
+
+  const STYLES = {
+    small: {
+      height: 8,
+      padding: 0,
+    },
+    medium: {
+      height: 12,
+      padding: 0,
+    },
+    large: {
+      height: 24,
+      padding: 2,
+    },
+  };
+  const style = STYLES[size];
+  if (!style) {
+    throw new Error("The size you gave as input is not correct");
+  }
+  return (
+    <OuterBar
+      style={{
+        "--height": style.height + "px",
+        "--padding": style.padding + "px",
+      }}
+      aria-valuenow={value}
+      aria-label="progressbar"
+      role="progressbar"
+    >
+      <VisuallyHidden value={value}></VisuallyHidden>
+
+      <FilledBar
+        style={
+          {
+           "--width": value + "%", 
+           "--padding": style.padding + "px",
+           "--radius": value === 99? '4px': '8px 0px 0px 8px'
+          }}
+      ></FilledBar>
+    </OuterBar>
+  );
 };
+
+const OuterBar = styled.div`
+  background: ${COLORS.gray50};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 8px;
+  width: 370px;
+  height: var(--height);
+  overflow: hidden;
+  padding: var(--padding);
+`;
+
+const FilledBar = styled.div`
+  background: ${COLORS.primary};
+  border-radius: var(--radius);
+  width: var(--width);
+  height: 100%;
+  padding: var(--padding);
+`;
 
 export default ProgressBar;
